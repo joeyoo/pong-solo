7ad74fdebb07198d234f8ba0df766ee99ff4e85d
notjoeyoo <joseph_yoo@brown.edu>
2016-05-17 23:55:41 -0400

    important changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-$(document).foundation()
+$(document).ready(function() {
+  $(document).foundation();
+  $("button").click(function() {
+    $(this).append("<div class='flex-video widescreen'><iframe width='420' height='315' src='https://www.youtube.com/v/gER7xe11nz8?rel=0&autoplay=1' frameborder='0' allowfullscreen></iframe></div>");
+  });
+});
